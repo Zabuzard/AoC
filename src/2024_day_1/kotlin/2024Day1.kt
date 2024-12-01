b62bde3d041ec3aabbfbea0fd7e6b7e2b0aa3811
Zabuzard <zabuza.dev@gmail.com>
2024-12-01 18:25:35 +0100

    day 1 part 1

--- src/2024_day_1/kotlin/2024Day1.kt
+++ src/2024_day_1/kotlin/2024Day1.kt
@@ -1,5 +1,17 @@
+import kotlin.math.abs
+
 // AOC Year 2024 Day 1
 fun main() {
     val lines = {}::class.java.getResourceAsStream("input.txt")!!.bufferedReader().readLines()
 
+    val originalPairs = lines.map { it.split(Regex("\\s+")) }
+        .map { it[0].toInt() to it[1].toInt() }
+
+    val leftSide = originalPairs.map { it.first }.sorted()
+    val rightSide = originalPairs.map { it.second }.sorted()
+
+    val sum = leftSide.zip(rightSide)
+        .sumOf { abs(it.first - it.second) }
+
+    println("Sum is $sum")
 }
