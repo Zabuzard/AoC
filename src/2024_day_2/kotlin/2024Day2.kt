ca9f24467637bf4727ef9665a0c2e663358cca5e
Zabuzard <zabuza.dev@gmail.com>
2024-12-02 10:37:05 +0100

    day 2 minor improvement

--- src/2024_day_2/kotlin/2024Day2.kt
+++ src/2024_day_2/kotlin/2024Day2.kt
@@ -15,23 +15,22 @@ fun main() {
 fun String.parseReport() = Report(split(" ").map { it.toInt() })
 
 data class Report(val levels: List<Int>) {
-    private fun areLevelsSafe(levels: List<Int>): Boolean {
-        val isAscending = levels[0] < levels[1]
-        return levels.zipWithNext()
-            .all { (left, right) ->
-                val isDistOkay = abs(left - right) in 1..3
-                val isOrderOkay = if (isAscending) left < right else left > right
-                isDistOkay && isOrderOkay
-            }
-    }
-
-    fun isSafe() = areLevelsSafe(levels)
+    fun isSafe() = levels.isSafe()
 
     fun isSafeWithDampener(): Boolean {
         if (isSafe()) return true
         return levels.indices.any { indexToRemove ->
-            val dampenedLevels = levels.filterIndexed { i, _ -> i != indexToRemove }
-            areLevelsSafe(dampenedLevels)
+            levels.filterIndexed { i, _ -> i != indexToRemove }.isSafe()
         }
     }
+}
+
+private fun List<Int>.isSafe(): Boolean {
+    val isAscending = this[0] < this[1]
+    return this.zipWithNext()
+        .all { (left, right) ->
+            val isDistOkay = abs(left - right) in 1..3
+            val isOrderOkay = if (isAscending) left < right else left > right
+            isDistOkay && isOrderOkay
+        }
 }
