aa3c2839c66942407193235862bddd180272395e
Zabuzard <zabuza.dev@gmail.com>
2024-12-03 08:48:55 +0100

    day 3 part 2

--- src/2024_day_3/kotlin/2024Day3.kt
+++ src/2024_day_3/kotlin/2024Day3.kt
@@ -2,17 +2,38 @@
 fun main() {
     val lines = {}::class.java.getResourceAsStream("input.txt")!!.bufferedReader().readLines()
 
-    val pattern = "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex()
+    val pattern = "mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)".toRegex()
+
+    var areInstructionsEnabled = true
 
     val result = lines.asSequence()
         .flatMap { pattern.findAll(it) }
         .map { it.groupValues }
-        .map { it[1].toInt() to it[2].toInt() }
-        .map { (a, b) -> Instruction(a, b) }
-        .sumOf { it.multiply() }
+        .mapNotNull {
+            when (it[0]) {
+                "do()" -> {
+                    areInstructionsEnabled = true
+                    null
+                }
+
+                "don't()" -> {
+                    areInstructionsEnabled = false
+                    null
+                }
+
+                else -> {
+                    if (areInstructionsEnabled) {
+                        Instruction(it[1].toInt(), it[2].toInt())
+                    } else {
+                        null
+                    }
+                }
+            }
+        }.sumOf { it.multiply() }
 
     println("Result is: $result")
 }
+
 
 data class Instruction(val a: Int, val b: Int) {
     fun multiply() = a * b
