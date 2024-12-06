0f58137a5528ea202f10c89de425968cfcd09420
Zabuzard <zabuza.dev@gmail.com>
2024-12-06 15:21:07 +0100

    day 6 part 2

--- src/2024_day_6/kotlin/2024Day6.kt
+++ src/2024_day_6/kotlin/2024Day6.kt
@@ -4,11 +4,34 @@ fun main() {
 
     val grid = lines.map { it.toList() }
 
-    var guardPosition =
+    val guardPosition =
         grid.flatMapIndexed { y, row -> row.mapIndexed { x, c -> Position(x, y) to c } }
-            .find { it.second == '^' }!!.first
-    var guardDirection = Direction.UP
-    var guardPositionWithDirection = PositionWithDirection(guardPosition.x, guardPosition.y, guardDirection)
+            .find { it.second == '^' }!!
+            .first.withDirection(Direction.UP)
+    val guardRoute = findGuardRoute(grid, guardPosition)!!
+
+
+    printGrid(grid, guardRoute)
+    println("Guard route: ${guardRoute.size}")
+
+    val possibleBlockPositions = guardRoute.toMutableSet() - guardPosition.withoutDirection()
+    val blockPositions = possibleBlockPositions.mapNotNull { blockPos ->
+        val gridWithBlock = grid.map { it.toMutableList() }.toList()
+        gridWithBlock[blockPos.y][blockPos.x] = '#'
+        if (findGuardRoute(gridWithBlock, guardPosition) == null) {
+            blockPos
+        } else {
+            null
+        }
+    }.toSet()
+
+    println("Possible block positions: ${blockPositions.size}")
+}
+
+fun findGuardRoute(grid: List<List<Char>>, guardStart: PositionWithDirection): Set<Position>? {
+    var guardPosition = guardStart.withoutDirection()
+    var guardDirection = guardStart.direction
+    var guardPositionWithDirection = guardStart
 
     val guardRoute = mutableSetOf<Position>()
     val guardRouteWithDirection = mutableSetOf<PositionWithDirection>()
@@ -18,26 +41,23 @@ fun main() {
         guardRouteWithDirection += guardPositionWithDirection
         val nextPosition = guardPosition.move(guardDirection)
 
-        val directionIfBlocked = guardDirection.turnRight()
-        val positionIfBlocked = guardPosition.move(directionIfBlocked)
-        val guardIfBlocked = PositionWithDirection(positionIfBlocked.x, positionIfBlocked.y, directionIfBlocked)
-        if (guardRouteWithDirection.contains(guardIfBlocked)) {
-            possibleBlockPositions += nextPosition
-        }
-
         when (grid.getOrNull(nextPosition.y)?.getOrNull(nextPosition.x)) {
             '#' -> guardDirection = guardDirection.turnRight()
             '.', '^' -> {
                 guardPosition = nextPosition
-                guardPositionWithDirection = PositionWithDirection(guardPosition.x, guardPosition.y, guardDirection)
+                guardPositionWithDirection = guardPosition.withDirection(guardDirection)
+                possibleBlockPositions += nextPosition
+
+                if (guardPositionWithDirection in guardRouteWithDirection) {
+                    // Cycle detected
+                    return null
+                }
             }
+
             null -> break
         }
     }
-
-    printGrid(grid, guardRoute)
-    println("Guard route: ${guardRoute.size}")
-    println("Possible block positions: ${possibleBlockPositions.size}")
+    return guardRoute
 }
 
 fun printGrid(grid: List<List<Char>>, guardRoute: Set<Position>) {
@@ -70,6 +90,8 @@ data class Position(val x: Int, val y: Int) {
             Direction.LEFT -> -1 to 0
             Direction.RIGHT -> 1 to 0
         }.let { Position(x + it.first, y + it.second) }
+
+    fun withDirection(direction: Direction) = PositionWithDirection(x, y, direction)
 }
 
 data class PositionWithDirection(val x: Int, val y: Int, val direction: Direction) {
@@ -80,4 +102,6 @@ data class PositionWithDirection(val x: Int, val y: Int, val direction: Directio
             Direction.LEFT -> -1 to 0
             Direction.RIGHT -> 1 to 0
         }.let { PositionWithDirection(x + it.first, y + it.second, direction) }
+
+    fun withoutDirection() = Position(x, y)
 }
