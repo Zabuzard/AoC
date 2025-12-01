f00ec95385c79957a804a436bbd9fd3db4f2ca5b
Zabuzard <zabuza.dev@gmail.com>
2025-12-01 08:19:22 +0100

    day 1 part 1

--- src/2025_day_1/kotlin/2025Day1.kt
+++ src/2025_day_1/kotlin/2025Day1.kt
@@ -1,6 +1,32 @@
 // AOC Year 2025 Day 1
 fun main() {
     val lines = {}::class.java.getResourceAsStream("input.txt")!!.bufferedReader().readLines()
-    
-    println(lines)
+
+    var passwordCount = 0
+    val safe = Safe()
+    lines.map { it.toTurns() }.forEach {
+        safe.turnRight(it)
+        if (safe.pos == 0) {
+            passwordCount++
+        }
+    }
+
+    println("Password is: $passwordCount")
+}
+
+fun String.toTurns() =
+    this.substring(1).toInt()
+        .let { if (this[0] == 'L') -it else it }
+
+class Safe() {
+    var pos = 50
+        private set
+
+    fun turnRight(turns: Int) {
+        pos += turns
+        pos %= 100
+        if (pos < 0) {
+            pos += 100
+        }
+    }
 }
