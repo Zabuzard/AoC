4464ea3c3fd18a749ed756a9321277f2ef180d2e
Zabuzard <zabuza.dev@gmail.com>
2025-12-01 08:42:43 +0100

    day 1 part 2

--- src/2025_day_1/kotlin/2025Day1.kt
+++ src/2025_day_1/kotlin/2025Day1.kt
@@ -1,17 +1,20 @@
+import kotlin.math.abs
+
 // AOC Year 2025 Day 1
 fun main() {
     val lines = {}::class.java.getResourceAsStream("input.txt")!!.bufferedReader().readLines()
 
-    var passwordCount = 0
+    var passwordCountPart1 = 0
     val safe = Safe()
     lines.map { it.toTurns() }.forEach {
         safe.turnRight(it)
         if (safe.pos == 0) {
-            passwordCount++
+            passwordCountPart1++
         }
     }
 
-    println("Password is: $passwordCount")
+    println("Part 1 Password is: $passwordCountPart1")
+    println("Part 2 Password is: ${safe.movedThroughZero}")
 }
 
 fun String.toTurns() =
@@ -19,14 +22,33 @@ fun String.toTurns() =
         .let { if (this[0] == 'L') -it else it }
 
 class Safe() {
+    var movedThroughZero = 0
+        private set
     var pos = 50
         private set
 
     fun turnRight(turns: Int) {
-        pos += turns
+        //val posBefore = pos
+
+        val fullRotations = abs(turns) / 100
+        movedThroughZero += fullRotations
+
+        val wasAtZero = pos == 0
+        val remainingTurns = turns % 100
+        pos += remainingTurns
+
+        if (pos !in 1..<100) {
+            movedThroughZero++
+        }
+        if (wasAtZero && pos < 0) {
+            movedThroughZero--
+        }
+
         pos %= 100
         if (pos < 0) {
             pos += 100
         }
+
+        //println("Turns $turns, Pos $posBefore -> $pos, moved zero $movedThroughZero")
     }
 }
